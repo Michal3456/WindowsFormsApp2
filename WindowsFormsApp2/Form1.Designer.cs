2893ab357f9308567dd051fd691f7fe0921744c5
Michal3456 <troianorum@wp.pl>
2017-12-27 17:52:39 +0100

    Dodaj pliki projektów.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNazwisko = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtImie = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHaslo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNick = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtKontakt = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtPowtorz_haslo = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNazwisko
+            // 
+            this.txtNazwisko.Location = new System.Drawing.Point(244, 99);
+            this.txtNazwisko.Multiline = true;
+            this.txtNazwisko.Name = "txtNazwisko";
+            this.txtNazwisko.Size = new System.Drawing.Size(100, 22);
+            this.txtNazwisko.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(128, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nazwisko";
+            // 
+            // txtImie
+            // 
+            this.txtImie.Location = new System.Drawing.Point(244, 57);
+            this.txtImie.Multiline = true;
+            this.txtImie.Name = "txtImie";
+            this.txtImie.Size = new System.Drawing.Size(100, 22);
+            this.txtImie.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(128, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 17);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Imie";
+            // 
+            // txtHaslo
+            // 
+            this.txtHaslo.Location = new System.Drawing.Point(244, 277);
+            this.txtHaslo.Multiline = true;
+            this.txtHaslo.Name = "txtHaslo";
+            this.txtHaslo.Size = new System.Drawing.Size(100, 22);
+            this.txtHaslo.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(128, 283);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Haslo";
+            // 
+            // txtNick
+            // 
+            this.txtNick.Location = new System.Drawing.Point(244, 235);
+            this.txtNick.Multiline = true;
+            this.txtNick.Name = "txtNick";
+            this.txtNick.Size = new System.Drawing.Size(100, 22);
+            this.txtNick.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(128, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Imie";
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(244, 188);
+            this.txtAdres.Multiline = true;
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(100, 22);
+            this.txtAdres.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(128, 194);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 17);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Adres";
+            // 
+            // txtKontakt
+            // 
+            this.txtKontakt.Location = new System.Drawing.Point(244, 146);
+            this.txtKontakt.Multiline = true;
+            this.txtKontakt.Name = "txtKontakt";
+            this.txtKontakt.Size = new System.Drawing.Size(100, 22);
+            this.txtKontakt.TabIndex = 9;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(128, 152);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 17);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Kontakt";
+            // 
+            // txtPowtorz_haslo
+            // 
+            this.txtPowtorz_haslo.Location = new System.Drawing.Point(244, 320);
+            this.txtPowtorz_haslo.Multiline = true;
+            this.txtPowtorz_haslo.Name = "txtPowtorz_haslo";
+            this.txtPowtorz_haslo.Size = new System.Drawing.Size(100, 22);
+            this.txtPowtorz_haslo.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(128, 326);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 17);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Powtorz_haslo";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(73, 211);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(472, 17);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "__________________________________________________________";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(244, 368);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(100, 23);
+            this.btnSubmit.TabIndex = 19;
+            this.btnSubmit.Text = "Zatwirdzam";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Zamknij";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(500, 418);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Logowanie-->";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(350, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 17);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "*";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(350, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 17);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "*";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label11.Location = new System.Drawing.Point(166, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(287, 44);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "REJESTRACJA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(614, 499);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtPowtorz_haslo);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtHaslo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNick);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtKontakt);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtNazwisko);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtImie);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "Rejsestracja lub logowanie";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtNazwisko;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtImie;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHaslo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNick;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtKontakt;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtPowtorz_haslo;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
