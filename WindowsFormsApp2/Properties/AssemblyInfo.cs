d93e0ffdc07bbf84b6c3aec41c6ff5a0705080c3
Michal3456 <troianorum@wp.pl>
2017-12-29 20:22:43 +0100

    0.2.1

--- WindowsFormsApp2/Properties/AssemblyInfo.cs
+++ WindowsFormsApp2/Properties/AssemblyInfo.cs
@@ -14,6 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+
 // Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
 // dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
 // COM, ustaw wartość true dla atrybutu ComVisible tego typu.
